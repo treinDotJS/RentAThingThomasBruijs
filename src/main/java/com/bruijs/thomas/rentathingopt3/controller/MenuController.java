2ad0302d55e0ed1d6dc66442cb94f58271dccdb4
treinDotJS <82215214+treinDotJS@users.noreply.github.com>
2022-05-27 22:50:51 +0200

    Add Menu view and controller

---
+++ src/main/java/com/bruijs/thomas/rentathingopt3/controller/MenuController.java
@@ -0,0 +1,28 @@
+package com.bruijs.thomas.rentathingopt3.controller;
+
+import com.bruijs.thomas.rentathingopt3.model.Medewerker;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MenuController {
+    private Medewerker medewerker;
+    @FXML
+    private Label medewerkerLabel;
+
+    @FXML
+    void logOut(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showBeheer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showOverzicht(ActionEvent event) {
+
+    }
+
+}

b79d9fc3e4d641436d4d8db29e995bdbbb8de9e1
treinDotJS <82215214+treinDotJS@users.noreply.github.com>
2022-05-30 18:22:50 +0200

    Add Boormachine class

---
+++ src/main/java/com/bruijs/thomas/rentathingopt3/model/Boormachine.java
@@ -0,0 +1,29 @@
+package com.bruijs.thomas.rentathingopt3.model;
+
+import java.time.LocalDate;
+
+public class Boormachine extends Product {
+    private final String merk, type;
+
+    public Boormachine(String merk, String type) {
+        this.merk = merk;
+        this.type = type;
+    }
+
+    @Override
+    double berekenHuur(LocalDate startDatum, LocalDate eindDatum, boolean isVerzekerd) {
+        int aantalDagen = startDatum.compareTo(eindDatum);
+
+        int huurPrijs = 5 * aantalDagen;
+        if (isVerzekerd) huurPrijs += aantalDagen;
+        return huurPrijs;
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public String getType() {
+        return type;
+    }
+}

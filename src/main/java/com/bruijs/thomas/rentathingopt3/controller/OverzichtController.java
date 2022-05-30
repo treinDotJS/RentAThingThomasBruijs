32e75d3a523bb8a9505c137880c5ca2748cc3393
treinDotJS <82215214+treinDotJS@users.noreply.github.com>
2022-05-30 22:55:34 +0200

    Add SetMedewerker interface for switching scenes with Medewerker

--- src/main/java/com/bruijs/thomas/rentathingopt3/controller/OverzichtController.java
+++ src/main/java/com/bruijs/thomas/rentathingopt3/controller/OverzichtController.java
@@ -1,13 +1,18 @@
 package com.bruijs.thomas.rentathingopt3.controller;
 
+import com.bruijs.thomas.rentathingopt3.model.Medewerker;
 import com.bruijs.thomas.rentathingopt3.model.product.Product;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 
-public class OverzichtController {
-
+public class OverzichtController implements SetMedewerker{
+    private Medewerker medewerker;
     @FXML
     private ListView<Product> productListView;
 
 
+    @Override
+    public void setMedewerker(Medewerker medewerker) {
+        this.medewerker = medewerker;
+    }
 }
--- src/main/java/com/bruijs/thomas/rentathingopt3/controller/SceneController.java
+++ src/main/java/com/bruijs/thomas/rentathingopt3/controller/SceneController.java
@@ -13,7 +13,7 @@ public class SceneController {
     public static final String MENU_VIEW_PATH = "/com/bruijs/thomas/rentathingopt3/view/MenuView.fxml";
     public static final String MENU_VIEW_TITLE = "Menu: %s";
     public static final String OVERZICHT_VIEW_PATH = "/com/bruijs/thomas/rentathingopt3/view/OverzichtView.fxml";
-    public static final String OVERZICHT_VIEW_TITLE = "/com/bruijs/thomas/rentathingopt3/view/OverzichtView.fxml";
+    public static final String OVERZICHT_VIEW_TITLE = "Overzicht: %s";
 
     public static void showScene(String scenePath, String title, Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SceneController.class.getResource(scenePath));
@@ -27,7 +27,7 @@ public class SceneController {
         FXMLLoader fxmlLoader = new FXMLLoader(SceneController.class.getResource(scenePath));
 
         Scene scene = new Scene(fxmlLoader.load());
-        MenuController controller = fxmlLoader.getController();
+        SetMedewerker controller = fxmlLoader.getController();
         controller.setMedewerker(medewerker);
 
         stage.setTitle(String.format(title, medewerker.getGebruikersnaam()));

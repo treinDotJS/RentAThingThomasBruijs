b88180e1ce840c58e99f2c49a248c127b8799820
treinDotJS <82215214+treinDotJS@users.noreply.github.com>
2022-05-30 22:30:10 +0200

    Add overzicht scene

---
+++ src/main/java/com/bruijs/thomas/rentathingopt3/controller/OverzichtController.java
@@ -0,0 +1,13 @@
+package com.bruijs.thomas.rentathingopt3.controller;
+
+import com.bruijs.thomas.rentathingopt3.model.product.Product;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class OverzichtController {
+
+    @FXML
+    private ListView<Product> productListView;
+
+
+}

4384bcd81c7684b9d591873a7ff2b6b2ca095992
treinDotJS <82215214+treinDotJS@users.noreply.github.com>
2022-05-30 22:30:41 +0200

    Update showScene method

--- src/main/java/com/bruijs/thomas/rentathingopt3/controller/SceneController.java
+++ src/main/java/com/bruijs/thomas/rentathingopt3/controller/SceneController.java
@@ -8,24 +8,30 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class SceneController {
-    public static final String LOGIN_VIEW = "/com/bruijs/thomas/rentathingopt3/view/LoginView.fxml";
-    public static final String MENU_VIEW = "/com/bruijs/thomas/rentathingopt3/view/MenuView.fxml";
-    public static void showScene(String scenePath, Stage stage) throws IOException {
+    public static final String LOGIN_VIEW_PATH = "/com/bruijs/thomas/rentathingopt3/view/LoginView.fxml";
+    public static final String LOGIN_VIEW_TITLE = "Login";
+    public static final String MENU_VIEW_PATH = "/com/bruijs/thomas/rentathingopt3/view/MenuView.fxml";
+    public static final String MENU_VIEW_TITLE = "Menu: %s";
+    public static final String OVERZICHT_VIEW_PATH = "/com/bruijs/thomas/rentathingopt3/view/OverzichtView.fxml";
+    public static final String OVERZICHT_VIEW_TITLE = "/com/bruijs/thomas/rentathingopt3/view/OverzichtView.fxml";
+
+    public static void showScene(String scenePath, String title, Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SceneController.class.getResource(scenePath));
         Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle(title);
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void showMenuScene(Stage stage, Medewerker medewerker) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(SceneController.class.getResource(MENU_VIEW));
+    public static void showScene(String scenePath, String title, Stage stage, Medewerker medewerker) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(SceneController.class.getResource(scenePath));
 
         Scene scene = new Scene(fxmlLoader.load());
-        MenuController menuController = fxmlLoader.getController();
-        menuController.setMedewerker(medewerker);
+        MenuController controller = fxmlLoader.getController();
+        controller.setMedewerker(medewerker);
 
+        stage.setTitle(String.format(title, medewerker.getGebruikersnaam()));
         stage.setScene(scene);
-        stage.setTitle("Menu: "+medewerker.getGebruikersnaam());
         stage.show();
     }
 }

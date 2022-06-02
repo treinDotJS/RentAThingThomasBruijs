dd25acd488e35b8e620c5ba93d5dae7739e2547c
treinDotJS <82215214+treinDotJS@users.noreply.github.com>
2022-06-02 13:38:39 +0200

    Add Beheer view and controller

---
+++ src/main/java/com/bruijs/thomas/rentathingopt3/controller/BeheerController.java
@@ -0,0 +1,26 @@
+package com.bruijs.thomas.rentathingopt3.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class BeheerController {
+
+    @FXML
+    private ListView<?> productListView;
+
+    @FXML
+    private Button toevoegenBtn;
+
+    @FXML
+    void nieuwProduct(ActionEvent event) {
+
+    }
+
+    @FXML
+    void previousScene(ActionEvent event) {
+
+    }
+
+}

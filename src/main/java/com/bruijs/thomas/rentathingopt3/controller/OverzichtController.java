b39a58232f749c58648c1bfdc3f8b5b540e20201
treinDotJS <82215214+treinDotJS@users.noreply.github.com>
2022-06-06 11:54:41 +0200

    Add doubleclick in overzichtList

--- src/main/java/com/bruijs/thomas/rentathingopt3/controller/OverzichtController.java
+++ src/main/java/com/bruijs/thomas/rentathingopt3/controller/OverzichtController.java
@@ -3,7 +3,6 @@ package com.bruijs.thomas.rentathingopt3.controller;
 import com.bruijs.thomas.rentathingopt3.data.ProductMagazijn;
 import com.bruijs.thomas.rentathingopt3.model.Medewerker;
 import com.bruijs.thomas.rentathingopt3.model.Observer;
-import com.bruijs.thomas.rentathingopt3.model.factory.ProductFactory;
 import com.bruijs.thomas.rentathingopt3.model.product.Product;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
@@ -17,10 +16,12 @@ import java.io.IOException;
 
 public class OverzichtController implements Observer, SetMedewerker{
     private Medewerker medewerker;
+    private Product previousSelectedProduct = null;
     @FXML
     private ListView<Product> productListView;
     @FXML
     private Button detailsBtn;
+
 
     @Override
     public void setMedewerker(Medewerker medewerker) {
@@ -29,8 +30,25 @@ public class OverzichtController implements Observer, SetMedewerker{
     }
 
     private void initialize() {
+        detailsBtn.setDisable(true);
+        productListView.getSelectionModel().selectedItemProperty().addListener(
+                (observableValue, product, t1) -> detailsBtn.setDisable(t1 == null)
+        );
+
+        productListView.setOnMouseClicked(mouseEvent -> setDoubleClick());
         ProductMagazijn.huidigMagazijn.attach(this);
         update();
+    }
+
+    private void setDoubleClick() {
+        Product newSelectedProduct = productListView.getSelectionModel().getSelectedItem();
+        if (previousSelectedProduct == newSelectedProduct) {
+            try {
+                showDetails();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else previousSelectedProduct = newSelectedProduct;
     }
 
     @FXML
@@ -40,9 +58,13 @@ public class OverzichtController implements Observer, SetMedewerker{
     }
 
     @FXML
-    void showDetails(ActionEvent event) throws IOException {
+    void onDetailBtnClick(ActionEvent event) throws IOException {
+        showDetails();
+    }
+
+    private void showDetails() throws IOException {
         Product product = productListView.getSelectionModel().getSelectedItem();
-        Stage stage = (Stage) ((Control) event.getSource()).getScene().getWindow();
+        Stage stage = (Stage) productListView.getScene().getWindow();
         SceneController.showScene(SceneController.DETAIL_VIEW_PATH, SceneController.DETAIL_VIEW_TITLE, stage, medewerker, product);
     }
 
